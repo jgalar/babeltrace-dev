21922de13b42d44ff33b7bd3040c50c58664c5e7
Ikaheimonen, JP <jp_ikaheimonen@mentor.com>
2013-05-02 12:07:50 +0000

    MinGW implementation of [v]asprintf

--- include/babeltrace/compat/stdio.h
+++ include/babeltrace/compat/stdio.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 
 #ifdef __MINGW32__
+
+#include <stdlib.h>
+#include <stdarg.h>
+
 static inline void flockfile (FILE * filehandle)
 {
 	return;
@@ -11,6 +15,40 @@ static inline void flockfile (FILE * filehandle)
 static inline void funlockfile(FILE * filehandle)
 {
 	return;
+}
+
+static inline
+int vasprintf(char ** strp, const char * fmt, va_list argv)
+{
+	int ret;
+
+	/* allocate even bigger chunks of memory until the string fits */
+	char * buf;
+	size_t size = 100;
+	for(;;) {
+		buf = malloc(size);
+		if (buf == (char *)0) {
+			ret = -1;
+			break;
+		}
+		ret = vsnprintf(buf, size, fmt, argv);
+		if (ret < size)
+			break;
+		size = size + 100;
+		free(buf);
+	}
+	return ret;
+}
+
+static inline
+int asprintf(char ** strp, const char *fmt, ...)
+{
+	int ret;
+	va_list argv;
+	va_start(argv, fmt);
+	ret = vasprintf(strp, fmt, argv);
+	va_end(argv);
+	return ret;
 }
 
 #endif
