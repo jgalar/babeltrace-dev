c21818abcbd3d28491294928a435600a73027845
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2013-06-21 15:38:14 -0400

    Change bt_ctf_clock's functions signature to return int (in case of error)
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/babeltrace/ctf-writer/clock.h
+++ include/babeltrace/ctf-writer/clock.h
@@ -29,13 +29,15 @@
  * SOFTWARE.
  */
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 struct bt_ctf_clock;
 
-extern bt_ctf_clock *bt_ctf_clock_create(const char *name);
+extern struct bt_ctf_clock *bt_ctf_clock_create(const char *name);
 
 extern void bt_ctf_clock_get(struct bt_ctf_clock *clock);
 
@@ -45,27 +47,27 @@ extern const char *bt_ctf_clock_get_name(struct bt_ctf_clock *clock);
 
 /* Optional */
 extern const char *bt_ctf_clock_get_description(struct bt_ctf_clock *clock);
-extern void bt_ctf_clock_set_description(struct bt_ctf_clock *clock,
+extern int bt_ctf_clock_set_description(struct bt_ctf_clock *clock,
 		const char *desc);
 
 /* Optional, Frequency in Hz, defaults to 1000000000 Hz (1ns) */
 extern uint64_t bt_ctf_clock_get_frequency(struct bt_ctf_clock *clock);
-extern void bt_ctf_clock_set_frequency(struct bt_ctf_clock *clock,
+extern int bt_ctf_clock_set_frequency(struct bt_ctf_clock *clock,
 		uint64_t freq);
 
 /* Optional, Precision in clock ticks, defaults to 1 */
 extern uint64_t bt_ctf_clock_get_precision(struct bt_ctf_clock *clock);
-extern void bt_ctf_clock_set_precision(struct bt_ctf_clock *clock,
+extern int bt_ctf_clock_set_precision(struct bt_ctf_clock *clock,
 		uint64_t precision);
 
 /* Optional, offset in seconds from POSIX.1 Epoch, 1970-01-01, defaults to 0 */
 extern uint64_t bt_ctf_clock_get_offset_s(struct bt_ctf_clock *clock);
-extern void bt_ctf_clock_set_offset_s(struct bt_ctf_clock *clock,
+extern int bt_ctf_clock_set_offset_s(struct bt_ctf_clock *clock,
 		uint64_t offset_s);
 
 /* Optional, offset in clock ticks from Epoch + offset_s, defaults to 0 */
 extern uint64_t bt_ctf_clock_get_offset(struct bt_ctf_clock *clock);
-extern void bt_ctf_clock_set_offset(struct bt_ctf_clock *clock,
+extern int bt_ctf_clock_set_offset(struct bt_ctf_clock *clock,
 		uint64_t offset);
 
 /*
@@ -73,14 +75,14 @@ extern void bt_ctf_clock_set_offset(struct bt_ctf_clock *clock,
  * other clocks. Defaults to FALSE.
  */
 extern int bt_ctf_clock_is_absolute(struct bt_ctf_clock *clock);
-extern void bt_ctf_clock_set_is_absolute(struct bt_ctf_clock *clock,
+extern int bt_ctf_clock_set_is_absolute(struct bt_ctf_clock *clock,
 		int is_absolute);
 
 /*
  * Set the current time in nanoseconds since the clock's origin (offset
  * attributes)
  */
-extern void bt_ctf_clock_set_time(struct bt_ctf_clock *clock, uint64_t time);
+extern int bt_ctf_clock_set_time(struct bt_ctf_clock *clock, uint64_t time);
 
 #ifdef __cplusplus
 }
