2906e00c70205a4bc97b04dfea2a4b2d1e5aa12e
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2013-07-08 14:11:15 -0400

    Add CTF Writer Trace implementation
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

---
+++ formats/ctf/writer/writer.c
@@ -0,0 +1,290 @@
+/*
+ * writer.c
+ *
+ * Babeltrace CTF Writer
+ *
+ * Copyright 2013 EfficiOS Inc. and Linux Foundation
+ *
+ * Author: Jérémie Galarneau <jeremie.galarneau@efficios.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <babeltrace/ctf-writer/writer.h>
+#include <babeltrace/ctf-writer/clock.h>
+#include <babeltrace/ctf-writer/writer-internal.h>
+#include <babeltrace/ctf-writer/functor-internal.h>
+#include <babeltrace/compiler.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <unistd.h>
+
+static void environment_variable_deallocate(gpointer element);
+static void clock_put(gpointer element);
+static void bt_ctf_writer_destroy(struct bt_ctf_ref *ref);
+
+static const char * reserved_keywords_str[] = {"align", "callsite" "const",
+	"char", "clock", "double", "enum", "env", "event", "floating_point",
+	"float", "integer", "int", "long", "short", "signed", "stream",
+	"string", "struct", "trace", "typealias", "typedef", "unsigned",
+	"variant", "void" "_Bool", "_Complex", "_Imaginary"};
+static GHashTable *reserved_keywords_set;
+static int init_done;
+static int reserved_refcount;
+
+struct bt_ctf_writer *bt_ctf_writer_create(const char *path)
+{
+	struct bt_ctf_writer *writer = g_new0(struct bt_ctf_writer, 1);
+	if (!writer) {
+		goto error;
+	}
+	writer->trace_dir_fd = -1;
+	bt_ctf_ref_init(&writer->ref_count, bt_ctf_writer_destroy);
+
+	if (path) {
+		int ret;
+		writer->path = g_string_new(path);
+		if (!writer->path) {
+			goto error_destroy;
+		}
+		ret = mkdir(path, S_IRWXU|S_IRWXG);
+		if (ret && errno != EEXIST) {
+			perror("mkdir");
+			goto error_destroy;
+		}
+		writer->trace_dir = opendir(path);
+		if (!writer->trace_dir) {
+			perror("opendir");
+			goto error_destroy;
+		}
+		writer->trace_dir_fd = dirfd(writer->trace_dir);
+		if (writer->trace_dir_fd < 0) {
+			perror("dirfd");
+			goto error_destroy;
+		}
+	}
+	writer->environment = g_ptr_array_new_with_free_func(
+		environment_variable_deallocate);
+	if (!writer->environment) {
+		goto error_destroy;
+	}
+
+	writer->clocks = g_ptr_array_new_with_free_func(clock_put);
+	if (!writer->clocks) {
+		goto error_destroy;
+	}
+	return writer;
+
+error_destroy:
+	bt_ctf_writer_destroy(&writer->ref_count);
+	writer = NULL;
+error:
+	return writer;
+
+}
+
+static void bt_ctf_writer_destroy(struct bt_ctf_ref *ref)
+{
+	if (!ref) {
+		return;
+	}
+	struct bt_ctf_writer *writer = container_of(ref, struct bt_ctf_writer,
+		ref_count);
+
+	if (writer->path) {
+		g_string_free(writer->path, TRUE);
+	}
+	if (writer->trace_dir_fd != -1) {
+		close(writer->trace_dir_fd);
+	}
+	if (writer->trace_dir) {
+		closedir(writer->trace_dir);
+	}
+	if (writer->environment) {
+		/* Individual elements are deallocated via GDestroyNotify */
+		g_ptr_array_free(writer->environment, TRUE);
+	}
+	if (writer->clocks) {
+		/* Clocks' refcounts are decreased via GDestroyNotify */
+		g_ptr_array_free(writer->clocks, TRUE);
+	}
+	g_free(writer);
+}
+
+struct bt_ctf_stream *bt_ctf_writer_add_stream(struct bt_ctf_writer *writer,
+		struct bt_ctf_stream *stream)
+{
+	return NULL;
+}
+
+int bt_ctf_writer_add_environment_field(struct bt_ctf_writer *writer,
+		const char *name,
+		const char *value)
+{
+	if (!writer || !name || !value) {
+		goto end;
+	}
+	struct environment_variable *var = g_new0(
+		struct environment_variable, 1);
+	if (!var) {
+		goto end;
+	}
+
+	var->name = g_string_new(name);
+	var->value = g_string_new(value);
+	if (!var->name || !var->value) {
+		goto error;
+	}
+	g_ptr_array_add(writer->environment, var);
+	return 0;
+
+error:
+	g_free(var->name);
+	g_free(var->value);
+	g_free(var);
+end:
+	return -1;
+}
+
+int bt_ctf_writer_add_clock(struct bt_ctf_writer *writer,
+		struct bt_ctf_clock *clock)
+{
+	int ret = 0;
+	if (!writer || !clock) {
+		ret = -1;
+		goto end;
+	}
+
+	/* Check for duplicate clocks */
+	struct search_query query = { .value = clock, .found = 0 };
+	g_ptr_array_foreach(writer->clocks, value_exists, &query);
+	if (query.found) {
+		ret = -1;
+		goto end;
+	}
+	bt_ctf_clock_get(clock);
+	g_ptr_array_add(writer->clocks, clock);
+end:
+	return ret;
+}
+
+char *bt_ctf_writer_get_metadata_string(struct bt_ctf_writer *writer)
+{
+	/* TODO */
+	return NULL;
+}
+
+int bt_ctf_writer_set_endianness(struct bt_ctf_writer *writer,
+		enum bt_ctf_byte_order endianness)
+{
+	if (!writer || writer->locked ||
+	    endianness < BT_CTF_BYTE_ORDER_NATIVE ||
+	    endianness >= BT_CTF_BYTE_ORDER_END) {
+		return -1;
+	}
+	/* This attribute can't change mid-trace */
+	if (!writer->locked) {
+		writer->endianness = endianness;
+	}
+	return 0;
+}
+
+void bt_ctf_writer_get(struct bt_ctf_writer *writer)
+{
+	if (!writer) {
+		return;
+	}
+	bt_ctf_ref_get(&writer->ref_count);
+}
+
+void bt_ctf_writer_put(struct bt_ctf_writer *writer)
+{
+	if (!writer) {
+		return;
+	}
+	bt_ctf_ref_put(&writer->ref_count);
+}
+
+int validate_identifier(const char *input_string)
+{
+	int ret = -1;
+	char *string = NULL;
+	if (!input_string) {
+		goto end;
+	}
+
+	string = strdup(input_string);
+	if (!string) {
+		goto end;
+	}
+
+	char *save_ptr, *token = strtok_r(string, " ", &save_ptr);
+	while (token) {
+		if (g_hash_table_contains(reserved_keywords_set,
+			GINT_TO_POINTER(g_quark_from_string(token)))) {
+			goto end;
+		}
+		token = strtok_r(NULL, " ", &save_ptr);
+	}
+	ret = 0;
+end:
+	free(string);
+	return ret;
+}
+
+static void environment_variable_deallocate(gpointer element)
+{
+	struct environment_variable *var = element;
+	g_string_free(var->name, TRUE);
+	g_string_free(var->value, TRUE);
+	g_free(var);
+}
+
+static void clock_put(gpointer element)
+{
+	bt_ctf_clock_put(element);
+}
+
+static __attribute__((constructor))
+void writer_init(void)
+{
+	reserved_refcount++;
+	if (init_done) {
+		return;
+	}
+	reserved_keywords_set = g_hash_table_new(g_direct_hash, g_direct_equal);
+
+	const size_t reserved_keywords_count =
+		sizeof(reserved_keywords_str) / sizeof(char *);
+	for (size_t i = 0; i < reserved_keywords_count; i++) {
+		g_hash_table_add(reserved_keywords_set,
+		GINT_TO_POINTER(g_quark_from_string(reserved_keywords_str[i])));
+	}
+	init_done = 1;
+}
+
+static __attribute__((destructor))
+void writer_finalize(void)
+{
+	if (--reserved_refcount == 0) {
+		g_hash_table_destroy(reserved_keywords_set);
+	}
+}
---
+++ include/babeltrace/ctf-writer/writer-internal.h
@@ -0,0 +1,60 @@
+#ifndef _BABELTRACE_CTF_WRITER_INTERNAL_H
+#define _BABELTRACE_CTF_WRITER_INTERNAL_H
+
+/*
+ * BabelTrace - CTF Writer
+ *
+ * CTF Writer Internal
+ *
+ * Copyright 2013 EfficiOS Inc. and Linux Foundation
+ *
+ * Author: Jérémie Galarneau <jeremie.galarneau@efficios.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <babeltrace/ctf-writer/ref-internal.h>
+#include <babeltrace/ctf-writer/writer.h>
+#include <babeltrace/babeltrace-internal.h>
+#include <glib.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+struct bt_ctf_writer {
+	struct bt_ctf_ref ref_count;
+	int locked; /* Protects attributes that can't be changed mid-trace */
+	GString *path;
+	enum bt_ctf_byte_order endianness;
+	DIR *trace_dir;
+	int trace_dir_fd;
+	GPtrArray *environment; /* Array of pointers to environment_variable */
+	GPtrArray *clocks; /* Array of pointers to bt_ctf_clock */
+	GPtrArray *streams; /* Array of pointers to bt_ctf_stream */
+	GPtrArray *stream_classes; /* Array of pointers to bt_ctf_stream_class */
+};
+
+struct environment_variable {
+	GString *name, *value;
+};
+
+/* Checks that the string is not a reserved keyword */
+BT_HIDDEN
+int validate_identifier(const char *string);
+
+#endif /* _BABELTRACE_CTF_WRITER_INTERNAL_H */
