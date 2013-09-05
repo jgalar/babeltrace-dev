8cad92554ea586f750279554a7b930d45c393274
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2013-09-04 23:36:54 -0400

    Add documentation to the Writer API
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/babeltrace/ctf-writer/writer.h
+++ include/babeltrace/ctf-writer/writer.h
@@ -46,36 +46,95 @@ enum bt_ctf_byte_order {
 	BT_CTF_BYTE_ORDER_END
 };
 
+/*
+ * bt_ctf_writer_create : create a writer instance
+ *
+ * Allocate a new writer that will produce a trace in the given path.
+ * The creation of a writer sets its reference count to 1.
+ *
+ * Returns an allocated writer on success and NULL on error.
+ */
 extern struct bt_ctf_writer *bt_ctf_writer_create(const char *path);
 
-extern void bt_ctf_writer_get(struct bt_ctf_writer *writer);
-
-extern void bt_ctf_writer_put(struct bt_ctf_writer *writer);
-
+/*
+ * bt_ctf_writer_create_stream : create a stream instance
+ *
+ * Allocate a new stream instance and register it to the writer. The creation of
+ * a stream sets its reference count to 1.
+ *
+ * Returns an allocated writer on success and NULL on error.
+ */
 extern struct bt_ctf_stream *bt_ctf_writer_create_stream(
 		struct bt_ctf_writer *writer,
 		struct bt_ctf_stream_class *stream_class);
 
+/*
+ * bt_ctf_writer_add_environment_field : add an environment field to the trace
+ *
+ * Add an environment field to the trace. The name and value parameters are
+ * copied.
+ *
+ * Returns 0 on success and a negative value on error.
+ */
 extern int bt_ctf_writer_add_environment_field(struct bt_ctf_writer *writer,
 		const char *name,
 		const char *value);
 
+/*
+ * bt_ctf_writer_add_clock : add a clock to the trace
+ *
+ * Add a clock to the trace. Clocks assigned to stream classes must be
+ * registered to the writer.
+ *
+ * Returns 0 on success and a negative value on error.
+ */
 extern int bt_ctf_writer_add_clock(struct bt_ctf_writer *writer,
 		struct bt_ctf_clock *clock);
 
-/* Caller assumes the ownership of the metadata string */
+/*
+ * bt_ctf_writer_get_metadata_string : get meta-data string
+ *
+ * Get the trace's TSDL meta-data. The caller assumes the ownership of the
+ * returned string.
+ *
+ * Returns the metadata string on success, NULL on error.
+ */
 extern char *bt_ctf_writer_get_metadata_string(struct bt_ctf_writer *writer);
 
 /*
- * Flush the trace's metadata to the metadata file.
- * Note that the metadata will be flushed automatically when the Writer instance
- * is released (last call to bt_ctf_writer_put).
+ * bt_ctf_writer_flush_metadata : flush the trace's metadata to disk
+ *
+ * Flush the trace's metadata to the metadata file. Note that the metadata will
+ * be flushed automatically when the Writer instance is released (last call to
+ * bt_ctf_writer_put).
  */
 extern void bt_ctf_writer_flush_metadata(struct bt_ctf_writer *writer);
 
-/* Defaults to the host's native endianness (BT_CTF_BYTE_ORDER_NATIVE) */
+/*
+ * bt_ctf_writer_set_byte_order : set a field type's byte order
+ *
+ * Set the trace's byte order. Defaults to BT_CTF_BYTE_ORDER_NATIVE,
+ * the host machine's endianness.
+ *
+ * Returns 0 on success and a negative value on error.
+ */
 extern int bt_ctf_writer_set_byte_order(struct bt_ctf_writer *writer,
 		enum bt_ctf_byte_order byte_order);
+
+/*
+ * bt_ctf_writer_get and bt_ctf_writer_put : increment and decrement the
+ * writer's reference count.
+ *
+ * These functions ensure that the writer won't be destroyed while it
+ * is in use. The same number of get and put (plus one extra put to
+ * release the initial reference done at creation) have to be done to
+ * destroy a writer.
+ *
+ * When the writer's reference count is decremented to 0 by a
+ * bt_ctf_writer_put, the writer is freed.
+ */
+extern void bt_ctf_writer_get(struct bt_ctf_writer *writer);
+extern void bt_ctf_writer_put(struct bt_ctf_writer *writer);
 
 #ifdef __cplusplus
 }
