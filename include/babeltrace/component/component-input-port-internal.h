c949d840b0a689afe337918bd5282294dfe05cbd
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2017-02-10 16:29:28 -0500

    Component port WIP

---
+++ include/babeltrace/component/component-input-port-internal.h
@@ -0,0 +1,44 @@
+#ifndef BABELTRACE_COMPONENT_INPUT_PORT_INTERNAL_H
+#define BABELTRACE_COMPONENT_INPUT_PORT_INTERNAL_H
+
+/*
+ * BabelTrace - Babeltrace Component Input Port Internal
+ *
+ * Copyright 2017 Jérémie Galarneau <jeremie.galarneau@efficios.com>
+ *
+ * Author: Jérémie Galarneau <jeremie.galarneau@efficios.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <babeltrace/component/component-port-internal.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct bt_input_port {
+	struct bt_port base;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BABELTRACE_COMPONENT_INPUT_PORT_INTERNAL_H */
